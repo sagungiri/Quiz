6760c297354993f05a340a1a5c476af8162ec10e
sagungiri <gireesagun998@gmail.com>
2019-12-02 19:51:15 +0545

    ok

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ var hello = document.querySelector('#submit');
 hello.addEventListener("click", function () {
 	getInfo();
 });
+
+
 // This is quiz time
 var questions = [
 	{
@@ -89,80 +91,5 @@ var questions = [
 		]
 	}
 ]
-
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('quiz')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('btn')
-
-let shuffledQuestions, currentQuestionIndex
-
-nextButton.addEventListener('click', () => {
-	currentQuestionIndex++
-	setNextQuestion()
-})
-
-function startGame() {
-	shuffledQuestions = questions.sort(() => Math.random() - .5)
-	currentQuestionIndex = 0
-	questionContainerElement.classList.remove('hide')
-	setNextQuestion()
-}
-
-function setNextQuestion() {
-	resetState()
-	showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-	questionElement.innerText = question.question
-	question.answers.forEach(answer => {
-		const button = document.createElement('button')
-		button.innerText = answer.text
-		button.classList.add('btn')
-		if (answer.correct) {
-			button.dataset.correct = answer.correct
-		}
-		button.addEventListener('click', selectAnswer)
-		answerButtonsElement.appendChild(button)
-	})
-}
-
-function resetState() {
-	clearStatusClass(document.body)
-	nextButton.classList.add('hide')
-	while (answerButtonsElement.firstChild) {
-		answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-	}
-}
-
-function selectAnswer(e) {
-	const selectedButton = e.target
-	const correct = selectedButton.dataset.correct
-	setStatusClass(document.body, correct)
-	Array.from(answerButtonsElement.children).forEach(button => {
-		setStatusClass(button, button.dataset.correct)
-	})
-	if (shuffledQuestions.length > currentQuestionIndex + 1) {
-		nextButton.classList.remove('hide')
-	} else {
-		startButton.innerText = 'Restart'
-		startButton.classList.remove('hide')
-	}
-}
-
-function setStatusClass(element, correct) {
-	clearStatusClass(element)
-	if (correct) {
-		element.classList.add('correct')
-	} else {
-		element.classList.add('wrong')
-	}
-}
-
-function clearStatusClass(element) {
-	element.classList.remove('correct')
-	element.classList.remove('wrong')
-}
 
 
